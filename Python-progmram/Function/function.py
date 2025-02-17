27a8b0f89971c18a570437ba604492d5120881da
Suvam-paul145 <suvampersonal555@gmail.com>
2025-02-17 12:04:56 +0530

    Python basics done

---
+++ Python-progmram/Function/function.py
@@ -0,0 +1,16 @@
+# add two no : 
+
+def add():
+    a = int(input("Enter first no: "))
+    b = int(input("Enter second no: "))
+    c = a + b
+    print(c)
+
+add()
+
+# factorial of number:
+def fact():
+    n = int(input("Enter the number: "))
+    fact
+    
+
