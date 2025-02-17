4a04c2c0ed5b80ab4261b332ff33fc537a273b36
Suvam-paul145 <suvampersonal555@gmail.com>
2025-02-17 11:18:50 +0530

    list done

---
+++ Python-progmram/print_even_no.py
@@ -0,0 +1,7 @@
+li = [5, 9, 57, 10, 4, 76, 8]
+# for list don't take the range 
+
+for i in li:
+    if i % 2 == 0:
+        print(i, end = " ")  #end = " use for taking all the elements in one line "
+
