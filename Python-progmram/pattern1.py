530965b576fa68c88d62a1e86b962e29f8b1c678
Suvam-paul145 <suvampersonal555@gmail.com>
2025-02-17 11:54:28 +0530

    pattern 2 done

--- Python-progmram/pattern1.py
+++ Python-progmram/pattern1.py
@@ -1,4 +1,27 @@
 for i in range (0,6):
     for j in range(0,i):
-        print("*",end = " ")
+        print("*",end = "")
     print("")
+
+# using one for lopp 
+for i in range(0,6):
+    print("*" * i) # * is multiply by i times
+
+# Make the pattern by using two for loop 
+# *
+# **
+# ***
+# ***
+# **
+# *
+
+n = int(input("Enter the number of rows: "))
+# it can take the string value if i don's type casting during input 
+
+for i in range(0,n):
+    print("*" * i)
+
+for j in range(n-1,0,-1):
+    print("*" *j)
+
+# Make the pattern by using one for loop 
