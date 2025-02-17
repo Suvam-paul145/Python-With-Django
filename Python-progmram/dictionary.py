d9455416d7c8cf8386ff577d92b8752320c9a248
Suvam-paul145 <suvampersonal555@gmail.com>
2025-02-17 11:13:39 +0530

    init

---
+++ Python-progmram/dictionary.py
@@ -0,0 +1,7 @@
+# dictonary 
+
+li1 = [1,1,2,3,4,5]
+
+li2 = [1,1,2,3,4,5]
+
+print(dict(li1,li2))
--- Python-progmram/touple.py
+++ Python-progmram/touple.py
@@ -12,3 +12,14 @@ li2 = [ 52545,4,544,4,54,54]
 
 li1.extend(li2)
 print(li1)
+
+# use removing all the elements of a list 
+
+print(li1.clear)
+
+#touple 
+
+t= [1,2,3,4,5,6,7,8,9]
+
+#slicing property use in python 
+print(t[3:])
