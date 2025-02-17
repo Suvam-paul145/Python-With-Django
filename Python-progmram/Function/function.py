f72e1b3d80bd2992fa4d2efa9577d438f49c9c81
Suvam-paul145 <suvampersonal555@gmail.com>
2025-02-17 12:26:12 +0530

    function arguement done

--- Python-progmram/Function/function.py
+++ Python-progmram/Function/function.py
@@ -1,5 +1,7 @@
+# Not parameterize =-> no arguement and no return type->
 # add two no : 
 
+import math as m
 def add():
     a = int(input("Enter first no: "))
     b = int(input("Enter second no: "))
@@ -9,8 +11,33 @@ def add():
 add()
 
 # factorial of number:
-def fact():
+def factorial():
     n = int(input("Enter the number: "))
-    fact
-    
+    print(m(n))
 
+# arguement and no return type :
+def greet(name):
+    print("Hello " , name, "....")
+    
+greet("Suvam")
+
+# add two no 
+def add(a,b):
+    a = int(input("Enter first no: "))
+    b = int(input("Enter second no: "))
+    c = (a + b)
+    print(c)
+add(5,36)
+
+# factorial of number:
+def factorial(n):
+    fact = 1
+    for i in range(1,n+1):
+        fact = fact * i
+    print(fact)
+
+# or using factoriAL FUNCTION 
+    def factorial(n):
+        fact = m.factorial(n)
+n = int(input("Enter nop for factorial : "))
+print(factorial(n))   
