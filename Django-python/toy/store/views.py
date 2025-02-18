c749b5e480547337fe62bce37b7a8765f3ab70dd
Suvam-paul145 <suvampersonal555@gmail.com>
2025-02-18 07:58:43 +0530

    demo 1 testing is on going

--- Django-python/toy/store/views.py
+++ Django-python/toy/store/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
-def test(requesst):
+def test(request):
     return HttpResponse('<h1>This is text page</h1>',
                         content_type='text/plain')
