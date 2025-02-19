606037f625ab79fb1f7f65e7fdd174d1ac061070
Suvam-paul145 <suvampersonal555@gmail.com>
2025-02-19 15:04:05 +0530

    Add user models and initial migrations for user2 and Meta

--- Django-python/toy/store/models.py
+++ Django-python/toy/store/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+class Meta(models.Model):
+    name = models.CharField(max_length=50)
+    email = models.EmailField 
+    password = models.CharField(max_length=50)
+    phno = models.IntegerField
 
-# Create your models here.
+class user2(models.Model):
+    fullname = models.CharField(max_length=50)
+    username = models.CharField(max_length=50)
+    email = models.EmailField
+    password = models.CharField(max_length=50)
+    class Meta: 
+        db_table = "user2"
