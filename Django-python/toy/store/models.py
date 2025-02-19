66f715edbea4048fb900f31f203c398f16352311
Suvam-paul145 <suvampersonal555@gmail.com>
2025-02-19 15:13:07 +0530

    done

--- Django-python/toy/store/models.py
+++ Django-python/toy/store/models.py
@@ -1,9 +1,11 @@
 from django.db import models
-class Meta(models.Model):
+class user(models.Model):
     name = models.CharField(max_length=50)
     email = models.EmailField 
     password = models.CharField(max_length=50)
     phno = models.IntegerField
+    class Meta:
+        db_table = "user"
 
 class user2(models.Model):
     fullname = models.CharField(max_length=50)
