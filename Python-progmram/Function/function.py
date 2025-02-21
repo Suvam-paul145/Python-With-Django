ebfda350efd96622fe783631f7feb6217a1afd56
Suvam-paul145 <suvampersonal555@gmail.com>
2025-02-21 07:55:55 +0530

    Refactor function definitions and improve code readability; update comments and enhance factorial calculation

--- Python-progmram/Function/function.py
+++ Python-progmram/Function/function.py
@@ -1,7 +1,8 @@
-# Not parameterize =-> no arguement and no return type->
-# add two no : 
+# Not parameterize =-> no argument and no return type->
 
 import math as m
+
+# add two no :
 def add():
     a = int(input("Enter first no: "))
     b = int(input("Enter second no: "))
@@ -13,31 +14,32 @@ add()
 # factorial of number:
 def factorial():
     n = int(input("Enter the number: "))
-    print(m(n))
+    print(m.factorial(n))
 
-# arguement and no return type :
+# argument and no return type :
 def greet(name):
-    print("Hello " , name, "....")
-    
+    print("Hello ", name, "....")
+
 greet("Suvam")
 
 # add two no 
-def add(a,b):
-    a = int(input("Enter first no: "))
-    b = int(input("Enter second no: "))
-    c = (a + b)
+def add(a, b):
+    c = a + b
     print(c)
-add(5,36)
+
+add(5, 36)
 
 # factorial of number:
 def factorial(n):
     fact = 1
-    for i in range(1,n+1):
-        fact = fact * i
+    for i in range(1, n + 1):
+        fact *= i
     print(fact)
 
-# or using factoriAL FUNCTION 
-    def factorial(n):
-        fact = m.factorial(n)
-n = int(input("Enter nop for factorial : "))
-print(factorial(n))   
+# or using factorial FUNCTION 
+def factorial_using_lib(n):
+    fact = m.factorial(n)
+    print(fact)
+
+n = int(input("Enter number for factorial: "))
+factorial(n)
